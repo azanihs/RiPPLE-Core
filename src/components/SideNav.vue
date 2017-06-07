94bc8370c0ebfb46668f54e09758b70dcf4b1bbc
CaptainAchilles <nicholas-achilles@hotmail.com>
2017-06-07 13:56:36 +1000

    Added nicer styling to sidenav menu

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,20 +1,86 @@
 <style scoped>
-    ul > li:first-of-type {
-        cursor: pointer;
+    .navbarContainer {
+        position: absolute;
+        height: 100%;
+        background-color: #1d323a;
+        color: #f2f2f2;
     }
-    ul > li:not(:first-of-type) {
+
+    .profileContainer hr {
+        width: 75%;
+        border: none;
+        border-bottom: 1px solid #274550;
+        margin: 1em auto;
+    }
+
+    .profileContainer {
+        text-align: center;
+    }
+
+    .profileContainer .imageContainer {
+        width: 100px;
+        height: auto;
+        padding: 10px;
+        margin: auto;
+    }
+    .imageContainer img {
+        width: 100%;
+        height: auto;
+        border-radius: 100%;
+        box-shadow: 2px 2px 2px #999;
+    }
+
+    .profileContainer h5 {
+        font-weight: normal;
+        font-size: 1em;
+        margin: 0px auto;
+    }
+
+    ul {
+        margin: 0px;
+        padding: 0px;
+        padding: 0px;
+    }
+
+    ul > li {
         list-style: none;
+        padding: 0px;
+        margin: 0px;
+
+    }
+
+    .linkButton {
+        width: 100%;
+        box-sizing: border-box;
+        margin: 0px;
+        color: #f2f2f2;
+        text-align: left;
+        border-radius: 0px;
+    }
+
+    /* Sets the background colour of the currently selected item */
+    .router-link-exact-active.router-link-active .linkButton {
+        background-color: #ffffff;
+        color: #111;
     }
 
 </style>
 
 <template>
-    <ul>
-        <li @click="toggleMenu">{{ showLinks ? "Hide Menu" : "Show Menu" }}
-        <li v-if="showLinks" v-for="link in links">
-            <router-link :to="link.href">{{ link.text }}</router-link>
-        </li>
-    </ul>
+    <div class="navbarContainer">
+        <div class="profileContainer">
+            <div class="imageContainer">
+                <img src="https://placebear.com/200/200" />
+            </div>
+            <h5>Nicholas Achilles</h5>
+            <hr />
+        </div>
+        <ul>
+            <li v-for="link in links">
+                <router-link :to="link.href"><md-button class="linkButton">{{ link.text }}</md-button></router-link>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script lang="ts">
@@ -35,10 +101,5 @@
             text: "Find Friends",
             href: "/view/friends"
         }];
-
-        showLinks: boolean = true;
-        toggleMenu() {
-            this.showLinks = !this.showLinks;
-        }
     }
 </script>
