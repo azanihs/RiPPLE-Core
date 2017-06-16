0d3acd4f59001f9a8dcfbce5acbb8e97d1e46e6f
CaptainAchilles <nicholas-achilles@hotmail.com>
2017-06-16 16:20:25 +1000

    Componentised the recommendations
    * Added card for recommendation
    * Added loops to make the code more consistent

---
+++ src/components/views/RecommendationCard.vue
@@ -0,0 +1,49 @@
+<template>
+<md-card >
+    <md-card-header>
+        <md-avatar>
+            <img :src="data.image" :alt="data.name">
+        </md-avatar>
+        <md-card-expand>
+            <md-card-actions>
+                <md-button>{{ data.name }}</md-button>
+                <span style="flex: 1"></span>
+                <md-button class="md-icon-button" md-expand-trigger>
+                    <md-icon>keyboard_arrow_down</md-icon>
+                </md-button>
+            </md-card-actions>
+            <md-card-content>
+                {{ data.bio }}
+            </md-card-content>
+        </md-card-expand>
+    </md-card-header>
+    <md-card-area>
+        <md-card-content>
+            <div class="card-reservation">
+                <md-button-toggle md-single class="md-button-group">
+                    <md-button> {{ data.time }}</md-button>
+                </md-button-toggle>
+                <span>
+                    <md-chip v-for="prof in data.proficiencies" :key="prof" class="topicChip">{{prof}}</md-chip>
+                </span>
+            </div>
+        </md-card-content>
+    </md-card-area>
+    <md-card-actions>
+        <md-button class="md-primary">ignore</md-button>
+        <md-button class="md-primary">Send Request</md-button>
+    </md-card-actions>
+</md-card>
+</template>
+
+<style scoped>
+</style>
+
+<script lang="ts">
+    import { Vue, Component, Prop } from "av-ts";
+
+    @Component()
+    export default class RecommendationCard extends Vue {
+        @Prop data;
+    }
+</script>
---
+++ src/components/views/Recommendations.vue
@@ -0,0 +1,55 @@
+<template>
+<md-list>
+    <md-list-item>
+        <md-icon>school</md-icon>
+        <span>COMPETENCIES</span>
+        <md-list-expand>
+            <md-layout md-gutter="16" md-flex="100">
+                <md-layout md-gutter v-for="peer in peers.recommendations" :key="peer.id">
+                    <recommendation-card :data="peer"/>
+                </md-layout>
+            </md-layout>
+        </md-list-expand>
+    </md-list-item>
+    <md-list-item>
+        <md-icon>people</md-icon>
+        <span>RECOMMENDATIONS</span>
+        <md-list-expand>
+            <md-layout md-gutter="16" md-flex="100">
+                <md-layout v-for="peer in peers.recommendations" :key="peer.id">
+                    <recommendation-card :data="peer"/>
+                </md-layout>
+            </md-layout>
+        </md-list-expand>
+    </md-list-item>
+    <md-list-item>
+        <md-icon>person_add</md-icon>
+        <span>REQUESTS</span>
+        <md-list-expand>
+            <md-layout md-gutter="16" md-flex="100">
+                <md-layout v-for="peer in peers.requests" :key="peer.id">
+                    <recommendation-card :data="peer" />
+                </md-layout>
+            </md-layout>
+        </md-list-expand>
+    </md-list-item>
+</md-list>
+</template>
+
+<style scoped>
+</style>
+
+<script lang="ts">
+    import { Vue, Component, Prop } from "av-ts";
+    import RecommendationCard from "./RecommendationCard";
+
+    @Component({
+        components: {
+            "recommendation-card": RecommendationCard
+        }
+    })
+    export default class Recommendations extends Vue {
+        @Prop peers;
+    }
+</script>
+
