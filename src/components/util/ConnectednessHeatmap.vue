eed0c28b8a999a306dfe75bb5fa6681dfd1026b6
CaptainAchilles <nicholas-achilles@hotmail.com>
2017-07-07 12:25:03 +1000

    Setting up initial structure

---
+++ src/components/util/ConnectednessHeatmap.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>
+        A
+    </p>
+</template>
+
+<style scoped>
+    
+</style>
+
+<script lang="ts">
+    import { Vue, Component } from "av-ts";
+
+    @Component()
+    export default class ConnectednessHeatmap extends Vue {
+
+    }
+</script>
--- src/components/views/DefaultView.vue
+++ src/components/views/DefaultView.vue
@@ -1,15 +1,19 @@
 <template>
-<p>
-DV
-</p>
+    <connectedness-heatmap />
 </template>
 
 <style scoped>
+    
 </style>
 
 <script lang="ts">
     import { Vue, Component } from "av-ts";
+    import ConnectednessHeatmap from "../util/ConnectednessHeatmap.vue";
 
-    @Component()
-    export default class DefaultView extends Vue {}
+    @Component({
+        components: {
+            "connectedness-heatmap": ConnectednessHeatmap
+        }
+    })
+    export default class DefaultView extends Vue { }
 </script>
