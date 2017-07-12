5c261260923b6bf408cbd35d98deaec7497f218e
CaptainAchilles <nicholas-achilles@hotmail.com>
2017-07-12 12:18:06 +1000

    Added UserBadge component as per the mockups in #4

--- src/components/util/CollectedBadges.vue
+++ src/components/util/CollectedBadges.vue
@@ -6,30 +6,11 @@
                    md-flex="33"
                    class="badgeGutter"
                    md-gutter>
-            <div class="badgeContainer">
-                <div class="badgeProgress"
-                     :class="{obtained: getUserBadge(badge)}">
-                    <md-icon>{{getBadgeIcon(badge)}}</md-icon>
-                    <md-spinner v-if="getUserBadge(badge) && getUserBadge(badge).progress >= 0"
-                                class="badgeSpinner"
-                                :md-stroke="2"
-                                :md-progress="getUserBadge(badge).progress"></md-spinner>
-                </div>
-                <div class="badgeDescription">
-                    <h4>{{badge.name}}</h4>
-                    <p>{{badge.description}}</p>
-                </div>
-            </div>
+            <user-badge :badge="badge"></user-badge>
         </md-layout>
     </md-layout>
 </template>
 
-<style>
-    .badgeProgress svg circle {
-        /* TODO: Move to theme */
-        stroke: #1d323a !important;
-    }
-</style>
 <style scoped>
     h2 {
         width: 100%;
@@ -44,76 +25,22 @@
     .badgeGutter:nth-of-type(3n) {
         padding-right: 0px !important;
     }
-    
-    .badgeContainer {
-        display: flex;
-        width: 100%;
-        align-items: center;
-        background-color: #fafafa;
-        margin-bottom: 16px;
-    }
-    
-    .badgeProgress {
-        position: relative;
-        height: 100%;
-        background-color: #eee;
-        align-items: center;
-        display: flex;
-        padding: 0px 1.25em;
-        border-right: 1px solid #e1e1e1;
-    }
-    
-    .badgeSpinner {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
-    
-    .badgeDescription {
-        padding-left: 8px;
-    }
-    
-    .badgeDescription h4 {
-        text-transform: capitalize;
-        margin-bottom: 0px;
-    }
-    
-    .badgeDescription p {
-        margin-top: 0.5em;
-    }
-    
-    .badgeProgress:not(.obtained) {
-        color: #ddd;
-    }
-    
-    .obtained {
-        color: #256;
-    }
 </style>
 
 <script lang="ts">
     import { Vue, Prop, Lifecycle, Component } from "av-ts";
+    import UserBadge from "../util/UserBadge.vue";
     import BadgeService from "../../services/BadgeService";
     import UserRepository from "../../repositories/UserRepository";
 
-    @Component()
+    @Component({
+        components: {
+            "user-badge": UserBadge
+        }
+    })
     export default class CollectedBadges extends Vue {
         get availableBadges() {
             return UserRepository.getAllAvailableBadges();
-        }
-
-        get userBadges() {
-            return UserRepository.getAllUserBadges();
-        }
-
-        getBadgeIcon(badge) {
-            return BadgeService.badgeToIcon(badge);
-        }
-
-        getUserBadge(badge) {
-            // TODO: This lookup is very slow, a hashmap or similar would be better.
-            return this.userBadges.find(x => x.badgeId == badge.id);
         }
     }
 </script>
