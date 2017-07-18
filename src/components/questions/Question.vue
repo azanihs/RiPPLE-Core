c990fea33e32bc4362db7fce80d4554e92508635
CaptainAchilles <nicholas-achilles@hotmail.com>
2017-07-18 17:29:39 +1000

    Added empty question component and route

---
+++ src/components/questions/Question.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>
+        {{$route.params.id}}</p>
+</template>
+
+<style scoped>
+    
+</style>
+
+
+<script lang="ts">
+    import { Vue, Component, Prop } from "av-ts";
+
+    @Component()
+    export default class Question extends Vue {
+
+
+    }
+</script>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import Main from "../main";
 import ProfileView from "../components/profile/ProfileView.vue";
 import FriendView from "../components/friends/FriendView.vue";
 import QuestionView from "../components/questions/QuestionView.vue";
+import Question from "../components/questions/Question.vue";
 import PeerView from "../components/views/PeerView.vue";
 
 Vue.use(VueRouter);
@@ -28,6 +29,10 @@ const routes = [{
 		path: "/view/peers",
 		name: "peers",
 		component: PeerView
+	}, {
+		path: "/view/question/:id",
+		name: "question",
+		component: Question
 	}]
 }];
 
