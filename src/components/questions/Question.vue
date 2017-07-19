92cc68f99543a7f447f556f357cb7c0600dab472
CaptainAchilles <nicholas-achilles@hotmail.com>
2017-07-19 11:51:41 +1000

    Added competency charts with colours

--- src/components/questions/Question.vue
+++ src/components/questions/Question.vue
@@ -1,43 +1,101 @@
 <template>
-    <div>
-        <scatter ref="scatter"
-                 :propData="competencies"></scatter>
-    </div>
+    <md-layout>
+        <overview-description>
+            <h2>Question Recommendations</h2>
+            <p>Have resources and exercises recommended to you in order to improve your course competencies</p>
+        </overview-description>
+        <md-layout md-flex="100"
+                   class="competencyContainer">
+            <div v-for="chart in competencies"
+                 :key="chart"
+                 class="chart">
+                <chart type="bar"
+                       :data="chart.data"
+                       :options="chart.options"></chart>
+            </div>
+        </md-layout>
+    </md-layout>
 </template>
 
 <style scoped>
-    div {
-        width: 25%;
-        height: 300px;
+    .competencyContainer {
+        margin-top: 1em;
+    }
+    
+    .chart {
+        flex: 1;
     }
 </style>
 
 
 <script lang="ts">
     import { Vue, Component, Lifecycle, Prop } from "av-ts";
-    import ScatterChart1D from "../charts/ScatterChart1D.vue";
+    import Chart from "../charts/Chart.vue";
+    import OverviewDescription from "../profile/OverviewDescription.vue";
     import UserService from "../../services/UserService";
 
     @Component({
         components: {
-            "scatter": ScatterChart1D
+            "chart": Chart,
+            "overview-description": OverviewDescription
         }
     })
     export default class Question extends Vue {
 
-        get competencies() {
-            return UserService.userCompetencies().data.datasets[1].data.map(x => ({
-                value: x,
-                fill: "#8bc34a"
-            }));
+        getBackgroundColor(value): Object {
+            if (value <= 25) {
+                return {
+                    backgroundColor: "rgba(208, 82, 82, 0.5)",
+                    borderColor: "rgba(208, 82, 82, 1)"
+                };
+            } else if (value > 25 && value <= 50) {
+                return {
+                    backgroundColor: "rgba(220, 143, 9, 0.5)",
+                    borderColor: "rgba(220, 143, 9, 1)"
+                };
+            } else if (value > 50 && value <= 75) {
+                return {
+                    backgroundColor: "rgba(34, 85, 102, 0.5)",
+                    borderColor: "rgba(34, 85, 102, 1)"
+                };
+            }
+            return {
+                backgroundColor: "rgba(34, 85, 102, 0.85)",
+                borderColor: "rgba(34, 85, 102, 1)"
+            };
         }
 
-        @Lifecycle
-        mounted() {
-            setTimeout(() => {
-                (this.$refs["scatter"] as ScatterChart1D).updateDimensions();
-                (this.$refs["scatter"] as ScatterChart1D).redraw();
-            }, 250);
+        get competencies() {
+            const data = UserService.userCompetencies().data;
+            const options = UserService.userCompetencies().options;
+
+            const userScores = data.datasets[1].data;
+
+            return userScores.map((x, i) => ({
+                data: {
+                    labels: [data.labels[i]],
+                    datasets: [{
+                        data: [x],
+                        label: "Your Qualification",
+                        borderWidth: 1,
+                        ...this.getBackgroundColor(x)
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                min: 0,
+                                max: 100,
+                                maxTicksLimit: 5
+                            }
+                        }]
+                    },
+                    legend: {
+                        display: false
+                    }
+                }
+            }));
         }
     }
 </script>
