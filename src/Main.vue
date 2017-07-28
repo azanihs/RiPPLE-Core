e9e751e6692a7802afc9ddef251671c026f6757b
CaptainAchilles <nicholas-achilles@hotmail.com>
2017-07-28 15:19:29 +1000

    Added icons and a responsive navigation bar

--- src/Main.vue
+++ src/Main.vue
@@ -1,40 +1,63 @@
 <template>
     <md-layout>
-        <md-layout class="sideNav">
-            <side-nav></side-nav>
+        <md-layout md-hide-xsmall md-hide-small md-hide-medium class="sideNavContainer">
+            <side-nav class="sideNav"></side-nav>
         </md-layout>
-        <md-layout class="pageContent">
+        <md-layout md-hide-xsmall md-hide-small md-hide-medium class="pageContent">
+            <router-view></router-view>
+        </md-layout>
+    
+        <md-layout md-hide-large-and-up class="mobileSideNavContainer">
+            <side-nav class="sideNav"></side-nav>
+        </md-layout>
+        <md-layout md-hide-large-and-up class="mobilePageContent">
             <router-view></router-view>
         </md-layout>
     </md-layout>
 </template>
 
 <style scoped>
-    .sideNav {
-        min-width: 12.5%;
-        flex: 0 1 12.5%;
-    }
-    
-    .pageContent {
-        margin-left: 1.25%;
-        min-width: 85%;
-        flex: 0 1 85%;
-    }
+.sideNavContainer {
+    min-width: 12.5%;
+    flex: 0 1 12.5%;
+    position: relative;
+}
+
+.pageContent {
+    margin-left: 1.25%;
+    min-width: 85%;
+    flex: 0 1 85%;
+}
+
+
+.mobileSideNavContainer {
+    min-width: 52px;
+    flex: 0 1 52px;
+    position: relative;
+}
+
+.mobilePageContent {}
+
+.sideNav {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+}
 </style>
 
 <style>
-    label {
-        cursor: pointer;
-    }
+label {
+    cursor: pointer;
+}
 </style>
 <script lang="ts">
-    import { Vue, Component } from "av-ts";
-    import SideNav from "./components/SideNav.vue";
+import { Vue, Component } from "av-ts";
+import SideNav from "./components/SideNav.vue";
 
-    @Component({
-        components: {
-            SideNav
-        }
-    })
-    export default class Main extends Vue { }
+@Component({
+    components: {
+        SideNav
+    }
+})
+export default class Main extends Vue { }
 </script>
--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,18 +1,32 @@
 <template>
     <div class="navbarContainer">
-        <div class="profileContainer">
-            <div class="imageContainer">
-                <img :src="personalAvatar" />
+        <md-layout md-flex="100" md-hide-xsmall md-hide-small md-hide-medium>
+            <div class="profileContainer">
+                <div class="imageContainer">
+                    <img :src="personalAvatar" />
+                </div>
+                <h5>Your Name</h5>
+                <h4>Course Name</h4>
             </div>
-            <h5>Your Name</h5>
-            <h4>Course Name</h4>
-            <hr />
-        </div>
+        </md-layout>
+        <md-layout md-hide-large-and-up>
+            <md-icon>menu</md-icon>
+        </md-layout>
+        <hr />
         <ul>
-            <li v-for="link in links"
-                :key="link.href">
+            <li v-for="link in links" :key="link.href">
                 <router-link :to="link.href">
-                    <md-button class="linkButton">{{ link.text }}</md-button>
+                    <md-layout md-hide-xsmall md-hide-small md-hide-medium>
+                        <md-button class="linkButton">
+                            <span>{{ link.text }}</span>
+                            <md-icon>{{link.icon}}</md-icon>
+                        </md-button>
+                    </md-layout>
+                    <md-layout md-hide-large-and-up>
+                        <md-button class="linkButton md-icon-button">
+                            <md-icon>{{link.icon}}</md-icon>
+                        </md-button>
+                    </md-layout>
                 </router-link>
             </li>
         </ul>
@@ -20,97 +34,117 @@
 </template>
 
 <style scoped>
-    .navbarContainer {
-        position: absolute;
-        height: 100%;
-        width: 12.5%;
-        background-color: #1d323a;
-        color: #f2f2f2;
-    }
-    
-    .profileContainer hr {
-        width: 75%;
-        border: none;
-        border-bottom: 1px solid #274550;
-        margin: 1em auto;
-    }
-    
-    .profileContainer {
-        text-align: center;
-    }
-    
-    .profileContainer .imageContainer {
-        width: 100px;
-        height: 100px;
-        padding: 10px;
-        margin: auto;
-    }
-    
-    .imageContainer img {
-        width: 100%;
-        height: auto;
-        border-radius: 100%;
-    }
-    
-    .profileContainer h5 {
-        font-weight: normal;
-        font-size: 1em;
-        margin: 0px auto;
-    }
-    
-    ul {
-        margin: 0px;
-        padding: 0px;
-        padding: 0px;
-    }
-    
-    ul>li {
-        list-style: none;
-        padding: 0px;
-        margin: 0px;
-    }
-    
-    .linkButton {
-        width: 100%;
-        box-sizing: border-box;
-        margin: 0px;
-        color: #f2f2f2;
-        text-align: left;
-        border-radius: 0px;
-    }
-    
-    .router-link-exact-active.router-link-active .linkButton,
-    .router-link-exact-active.router-link-active .linkButton:hover {
-        /* Sets the background colour of the currently selected item */
-        background-color: #ffffff;
-        color: #111;
-    }
-    
-    h4 {
-        font-size: 1em;
-        margin: 0px;
-        padding: 0px;
-        color: #aaa;
-    }
+.navbarContainer {
+    background-color: #1d323a;
+    color: #f2f2f2;
+}
+
+hr {
+    width: 75%;
+    border: none;
+    border-bottom: 1px solid #274550;
+    margin: 1em auto;
+}
+
+.profileContainer {
+    text-align: center;
+    margin: auto;
+}
+
+.profileContainer .imageContainer {
+    width: 100px;
+    height: 100px;
+    padding: 10px;
+    margin: auto;
+}
+
+.imageContainer img {
+    width: 100%;
+    height: auto;
+    border-radius: 100%;
+}
+
+.profileContainer h5 {
+    font-weight: normal;
+    font-size: 1em;
+    margin: 0px auto;
+}
+
+ul {
+    margin: 0px;
+    padding: 0px;
+    padding: 0px;
+}
+
+ul>li {
+    list-style: none;
+    padding: 0px;
+    margin: 0px;
+}
+
+.linkButton {
+    width: 100%;
+    box-sizing: border-box;
+    color: #f2f2f2;
+    text-align: left;
+    border-radius: 0px;
+
+
+    display: flex;
+    flex: 1;
+    justify-content: space-between;
+}
+
+.linkButton:not(.md-icon-button) {
+    margin-left: -1px;
+    margin-right: -1px;
+}
+
+.linkButton:not(.md-icon-button)>i {
+    margin-left: 0px;
+    margin-right: 0px;
+}
+
+.router-link-exact-active.router-link-active .linkButton.md-icon-button {
+    border-radius: 0px;
+    margin: 0px;
+}
+
+.router-link-exact-active.router-link-active .linkButton,
+.router-link-exact-active.router-link-active .linkButton:hover {
+    /* Sets the background colour of the currently selected item */
+    background-color: #ffffff;
+    color: #111;
+}
+
+h4 {
+    font-size: 1em;
+    margin: 0px;
+    padding: 0px;
+    color: #aaa;
+}
 </style>
 
 <script lang="ts">
-    import { Vue, Component, Prop } from "av-ts";
-    import PeerRepository from "../repositories/PeerRepository";
+import { Vue, Component, Prop } from "av-ts";
+import PeerRepository from "../repositories/PeerRepository";
 
-    @Component()
-    export default class SideNav extends Vue {
-        personalAvatar = PeerRepository.getMany(1)[0].image;
+@Component()
+export default class SideNav extends Vue {
+    personalAvatar = PeerRepository.getMany(1)[0].image;
 
-        links = [{
-            text: "Profile",
-            href: "/"
-        }, {
-            text: "View Questions",
-            href: "/view/questions"
-        }, {
-            text: "Connect To Peers",
-            href: "/view/peers"
-        }];
-    }
+    links = [{
+        text: "Profile",
+        href: "/",
+        icon: "widgets"
+    }, {
+        text: "View Questions",
+        href: "/view/questions",
+        icon: "directions"
+    }, {
+        text: "Connect To Peers",
+        href: "/view/peers",
+        icon: "group"
+    }];
+}
 </script>
