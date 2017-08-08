7f1706b337778bcd466da7bbbd0bdf62eb886efe
Nicholas Achilles <uqnachil@uq.edu.au>
2017-08-09 09:13:21 +1000

    Added leaderboard sorting

--- src/components/leaderboard/LeaderBoard.vue
+++ src/components/leaderboard/LeaderBoard.vue
@@ -1,32 +1,32 @@
 <template>
     <md-table-card class="table">
-        <md-table>
+        <md-table md-sort="reputation" md-sort-type="asc" @sort="sort">
             <md-table-header>
                 <md-table-row>
-                    <md-table-head>
+                    <md-table-head md-sort-by="name">
                         <md-icon>account_box</md-icon>
                         <span>Name</span>
                     </md-table-head>
-                    <md-table-head>
+                    <md-table-head md-sort-by="reputation">
                         <md-icon>star</md-icon>
                         <span>Reputation</span>
                     </md-table-head>
-                    <md-table-head>
+                    <md-table-head md-sort-by="questionsContributed">
                         <md-icon>library_add</md-icon>
                         <span>Questions Contributed</span>
                     </md-table-head>
-                    <md-table-head>
+                    <md-table-head md-sort-by="numberAnswers">
                         <md-icon>reply</md-icon>
                         <span>Questions Answered</span>
                     </md-table-head>
-                    <md-table-head>
+                    <md-table-head md-sort-by="numberComments">
                         <md-icon>message</md-icon>
                         <span>Comments</span>
                     </md-table-head>
                 </md-table-row>
             </md-table-header>
             <md-table-body>
-                <md-table-row v-for="user in mostReputableUsers" :key="user.id">
+                <md-table-row v-for="user in sortedUsers" :key="user.id">
                     <md-table-cell>{{ user.name }}</md-table-cell>
                     <md-table-cell>{{ user.reputation }}</md-table-cell>
                     <md-table-cell>{{ user.questionsContributed }}</md-table-cell>
@@ -52,8 +52,27 @@ import UserService from "../../services/UserService";
 @Component()
 export default class LeaderBoard extends Vue {
 
+    sortType: string = "";
+    reverse: boolean = false;
+
+    sort(item) {
+        this.sortType = item.name;
+        this.reverse = item.type == "desc";
+    }
+
     get mostReputableUsers() {
         return UserService.mostReputableUsers();
+    }
+
+    get sortedUsers() {
+        const users = this.mostReputableUsers;
+        const sortKey = this.sortType || "reputation";
+
+        users.sort((a, b) => a[sortKey] - b[sortKey]);
+        if (this.reverse) {
+            return users.reverse();
+        }
+        return users;
     }
 }
 </script>
