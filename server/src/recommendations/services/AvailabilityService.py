730f8eabef0ae8baa093bd297cc59c932294380b
Mark Belonogoff <mark.belonogoff@gmail.com>
2017-11-07 18:49:12 +1000

    Added HTTP response for availability count of course

--- server/src/recommendations/services/AvailabilityService.py
+++ server/src/recommendations/services/AvailabilityService.py
@@ -1,6 +1,8 @@
 from ..models import Day, Time, Availability
-
+from django.db.models import Count
 
 def get_user_availability(course_user):
-    availability = Availability.objects.filter(course_user=course_user)
-    return availability
+    return Availability.objects.filter(course_user=course_user)
+
+def get_course_availability(course):
+    return [x for x in Availability.objects.values('day', 'time').annotate(entries=Count('id')).order_by('day_id', 'time_id')]
