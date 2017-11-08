bfcad22750f05e97796f8b5af082142b4cfc04b9
Mark Belonogoff <mark.belonogoff@gmail.com>
2017-11-08 18:29:24 +1000

    Added server handling to update user availability

--- server/src/recommendations/services/AvailabilityService.py
+++ server/src/recommendations/services/AvailabilityService.py
@@ -1,8 +1,31 @@
 from ..models import Day, Time, Availability
 from django.db.models import Count
+from django.core.exceptions import ObjectDoesNotExist
+
 
 def get_user_availability(course_user):
     return Availability.objects.filter(course_user=course_user)
 
 def get_course_availability(course):
     return [x for x in Availability.objects.values('day', 'time').annotate(entries=Count('id')).order_by('day_id', 'time_id')]
+
+def update_availability(course_user, day_id, time_id, available):
+    # Check if availaibility exists
+    try:
+        availability = Availability.objects.get(course_user=course_user, day=day_id, time=time_id)
+        exists = True
+    except ObjectDoesNotExist:
+        exists = False
+
+    if not available and exists:
+        availability.delete()
+        return True
+
+    if available and not exists:
+        day = Day.objects.get(pk=day_id)
+        time = Time.objects.get(pk=time_id)
+        availability = Availability(course_user=course_user, day=day, time=time)
+        availability.save()
+        return True
+
+    return None
