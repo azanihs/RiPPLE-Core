5d28f24b8f9a4071f88e769d182e5eee5aa91a39
Nicholas Joseph <nicholas.joseph@uqconnect.edu.au>
2017-11-10 12:23:08 +1000

    Cleaned up code

--- migration/delete_db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$file = "pw_orig.db";
-	if(file_exists($file)){
-		unlink($file);
-	}
-
-	echo json_encode(array("result" => "success"));
-?>
--- migration/parser/parser.php
+++ migration/parser/parser.php
@@ -1,12 +1,11 @@
 <?php
-
+ini_set('max_execution_time', 0);
 // Load the DOM parser
 include_once('question.php');
 
 //Make the parent dir readable
-// shell_exec("chmod 773 ..");
+
 $file = $_FILES['file']['tmp_name'];
-// $file = "data1.html";
 
 $doc = new DOMDocument();
 libxml_use_internal_errors(true);
@@ -14,35 +13,22 @@ $doc->loadHTMLFile($file);
 libxml_clear_errors();
 
 $xpath = new DOMXPath($doc);
-
-// $html = file_get_html('dummy-data.html');            
-// $html = file_get_html('buggy4.html');            
-$db = setupDatabaseOrig();
-$dbMod = setupDatabaseMod();
-
-$schema = array(
-	'peerwise_id' => SQLITE3_INTEGER,
-	'created_on' => SQLITE3_BLOB,	
-	'question' => SQLITE3_BLOB,
-	'alternative_a' => SQLITE3_BLOB,
-	'alternative_b' => SQLITE3_BLOB,
-	'alternative_c' => SQLITE3_BLOB,
-	'alternative_d' => SQLITE3_BLOB,	
-	'alternative_e' => SQLITE3_BLOB,
-	'correct_alternative' => SQLITE3_BLOB,
-	'explanation' => SQLITE3_BLOB,
-	'tags' => SQLITE3_BLOB,
-	'author' => SQLITE3_BLOB,
-	'average_rating' => SQLITE3_INTEGER,
-	'average_difficulty' => SQLITE3_INTEGER,
-	'total_ratings' => SQLITE3_INTEGER
-);
-
-
 $idname = 'displayQuestionTable';
 $questionTable = $xpath->query("//table[@id='" . $idname . "']");
 
-$totalSaveCount = 0;
+function sendJSON($json) {  
+	$url = "http://localhost:8000/questions/add/";
+	$auth = "B45JJmX6wOFpYawMWCcVSWWyZ6pBo0GY";
+	$options = [
+		"http" => [
+			"header" => "Content-Type: application/json\r\nAccept: application/json\r\nAuthorization: $auth",
+			"method" => "POST",
+			"content" => $json
+		]
+	];
+	$context = stream_context_create($options);
+	$result = file_get_contents($url,false,$context);
+}
 
 function get_inner_html( $node ) 
 {
@@ -56,6 +42,8 @@ function get_inner_html( $node )
      
     return $innerHTML;
 }
+
+$questionsAdded = 0;
 
 foreach ($questionTable as $node) {
 	$question = new Question();
@@ -75,74 +63,12 @@ foreach ($questionTable as $node) {
 		}
 	}
 
+	$json = $question->question_as_json();
 
-
-	if ($question->question_as_json($schema) == TRUE) {
-		echo "ha";		
-	}
-	/*if( $question->save_to_db($db, $schema) == TRUE){
-		
-		$question->save_to_db($dbMod, $schema);
-		
-		$totalSaveCount++;
-	}*/
-}
-
-echo json_encode( array('totalSaveCount' => $totalSaveCount, 'result' => 'success' ) );
-
-
-
-
-
-function setupDatabaseOrig(){
-	$db = new SQLite3(__DIR__."/../pw_orig.db");
-
-	$db->exec('CREATE TABLE IF NOT EXISTS peerwise_questions (
-		id 				INTEGER PRIMARY KEY AUTOINCREMENT,
-		peerwise_id 	INTEGER UNIQUE, 
-		created_on 		BLOB,
-		question 		BLOB,
-		alternative_a	BLOB,
-		alternative_b	BLOB,
-		alternative_c	BLOB,
-		alternative_d	BLOB,
-		alternative_e	BLOB,
-		correct_alternative BLOB, 
-		explanation		BLOB,
-		tags 			BLOB,
-		author 			BLOB,
-		average_rating 		INTEGER,
-		average_difficulty 	INTEGER,
-		total_ratings 		INTEGER
-		)');
-
-	return $db;
-}
-
-function setupDatabaseMod(){
-	$db = new SQLite3("../pw_mod.db");
+	sendJson($json);
 	
-	$db->exec('CREATE TABLE IF NOT EXISTS peerwise_questions (
-		id 				INTEGER PRIMARY KEY AUTOINCREMENT,
-		peerwise_id 	INTEGER UNIQUE, 
-		created_on 		BLOB,
-		question 		BLOB,
-		alternative_a	BLOB,
-		alternative_b	BLOB,
-		alternative_c	BLOB,
-		alternative_d	BLOB,
-		alternative_e	BLOB,
-		correct_alternative BLOB, 
-		explanation		BLOB,
-		tags 			BLOB,
-		author 			BLOB,
-		average_rating 		INTEGER,
-		average_difficulty 	INTEGER,
-		total_ratings 		INTEGER,
-		quiz_topic BLOB DEFAULT dni
-		)');
-
-	return $db;
+	$questionsAdded++;
 }
 
+echo json_encode(array("result"=>"success", "questionsAdded"=>$questionsAdded));
 ?>
