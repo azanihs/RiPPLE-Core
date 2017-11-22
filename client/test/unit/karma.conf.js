8d8bdac94abca6e9513703bf613ddbcbaae831ba
Nicholas Achilles <uqnachil@uq.edu.au>
2017-11-22 11:34:35 +1000

    Fixed build

--- client/test/unit/karma.conf.js
+++ client/test/unit/karma.conf.js
@@ -2,6 +2,7 @@
 //   http://karma-runner.github.io/0.13/config/configuration-file.html
 // we are also using it with karma-webpack
 //   https://github.com/webpack/karma-webpack
+process.env.NODE_ENV = "development"
 const webpackConfig = require("../../build/webpack.dev.conf");
 const path = require("path");
 
