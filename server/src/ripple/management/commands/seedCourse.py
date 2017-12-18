d6bf42cb07fa004c8a70d16769b4723613919612
Diego Robles <diego_robles99@hotmail.com>
2017-12-18 14:27:56 +1000

    small print fix

--- server/src/ripple/management/commands/seedCourse.py
+++ server/src/ripple/management/commands/seedCourse.py
@@ -302,7 +302,7 @@ class Command(BaseCommand):
             index = 0
             for user in course_users:
                 studentAbility = abilities[choice(range(3))]
-                print("\t-Answering Course User:" + index + " Questions")
+                print("\t-Answering Course User:" + str(index) + " Questions")
                 index += 1
                 for i in range(0, 100):
                     make_question_responses(user, correct_distractors, incorrect_distractors, studentAbility)
